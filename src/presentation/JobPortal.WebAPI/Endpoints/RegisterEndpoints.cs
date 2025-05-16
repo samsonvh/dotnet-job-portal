16aa7d296152c5d72be6fc7aa43c20118d2e3b4a
samsonvh <samson.vh.20@gmail.com>
2025-05-16 18:07:09 +0700

    Add Carter routing for applicant and company registration
    - Implemented `RegisterEndpoints` class to handle registration routes for applicants and companies.
    - Updated `JobPortal.WebAPI.csproj` to include `Carter` package version `8.2.1`.
    - Registered Carter services in `Program.cs` and added logging for validators.
    - Configured HTTP request pipeline to map Carter routes.

---
+++ src/presentation/JobPortal.WebAPI/Endpoints/RegisterEndpoints.cs
@@ -0,0 +1,33 @@
+﻿using Carter;
+using JobPortal.Application.Users.Applicant.Commands.Register;
+using JobPortal.Application.Users.Company.Commands.Register;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobPortal.WebAPI.Endpoints
+{
+    public class RegisterEndpoints : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            var group = app.MapGroup("api/register");
+
+            group.MapPost("applicant", RegisterApplicant).WithName(nameof(RegisterApplicant));
+            group.MapPost("company", RegisterCompany).WithName(nameof(RegisterCompany));
+        }
+
+        private static async Task<IResult> RegisterApplicant([FromBody] ApplicantRegisterCommand command, [FromServices] ISender sender)
+        {
+            await sender.Send(command);
+
+            return Results.Ok();
+        }
+        private static async Task<IResult> RegisterCompany([FromBody] CompanyRegisterCommand command, [FromServices] ISender sender)
+        {
+            await sender.Send(command);
+
+            return Results.Ok();
+        }
+    }
+}
--- src/presentation/JobPortal.WebAPI/Program.cs
+++ src/presentation/JobPortal.WebAPI/Program.cs
@@ -1,6 +1,9 @@
+using Carter;
+using FluentValidation;
 using JobPortal.Application;
 using JobPortal.Infrastructure;
 using JobPortal.Infrastructure.Persistence;
+using JobPortal.WebAPI;
 using JobPortal.WebAPI.Middlewares;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -12,10 +15,23 @@ builder.Services.AddApplicationDependencies();
 builder.Services.AddInfrastructureDependencies();
 builder.Services.AddPersistenceDependencies(builder.Configuration);
 
+builder.Services.AddCarter();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+foreach (var serviceDescriptor in builder.Services
+    .Where(sd => sd.ServiceType.IsGenericType
+        && sd.ServiceType.GetGenericTypeDefinition() == typeof(IValidator<>)))
+{
+    Console.WriteLine($"Validator: {serviceDescriptor.ImplementationType?.Name} - Lifetime: {serviceDescriptor.Lifetime}");
+}
+
+builder.Host.UseDefaultServiceProvider((context, options) =>
+{
+    options.ValidateScopes = true;
+});
 
 var app = builder.Build();
 
@@ -31,6 +47,8 @@ app.UseHttpsRedirection();
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseAuthorization();
+
+app.MapCarter();
 
 app.MapControllers();
 
